e56d38ed6fdbdae94a6e87e17d870d4de8fceaa2
Nafish Ahmed <nafis.ahmed.dev@gmail.com>
2023-02-11 21:36:50 +0530

    fix

--- lib/screens/event_details_screen.dart
+++ lib/screens/event_details_screen.dart
@@ -19,18 +19,25 @@ class EventDetailsScreen extends StatefulWidget {
 }
 
 class _EventDetailsScreenState extends State<EventDetailsScreen> {
-  var _event = null;
-  var _venue = null;
-  var _artists = null;
+  var _event;
+  var _event_tickets;
+  var _venue;
+  var _artists;
   bool _loading = true;
+  var top = 0.0;
 
   fetchEvent() async {
-    int event_id = widget.event_id;
-    var response = await EventService.details(event_id);
+    setState(() {
+      _loading = true;
+    });
+    int eventId = widget.event_id;
+    var response = await EventService.details(eventId);
     setState(() {
       _event = response["event"];
+      _event_tickets = response["event_tickets"];
       _venue = response["venue"];
       _artists = response["artists"];
+      _loading = false;
     });
   }
   @override
@@ -44,41 +51,185 @@ class _EventDetailsScreenState extends State<EventDetailsScreen> {
     return StoreConnector<AppState, AppState>(
         converter: (store) => store.state,
         builder: (context, state) {
-          return Scaffold(
-              body:
-              CustomScrollView(
-                slivers: <Widget>[
-                  SliverAppBar(
-                    pinned: true,
-                    snap: false,
-                    floating: false,
-                    expandedHeight: 200,
-                    stretch: true,
-                    forceElevated: true,
-                    flexibleSpace: FlexibleSpaceBar(
-                      collapseMode: CollapseMode.pin,
-                      title: Text(_event?["name"] ?? ""),
-                      background: Image.network(CommonService.generateResourceUrl(_event?["cover_image"] ?? ""), fit: BoxFit.cover,),
-                    ),
-                  ),
-                  SliverToBoxAdapter(
 
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                          child: Html(data:  _event?["description"]??"",),
-                        ),
-
-                        Container(
-                          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                          child: Html(data:  _event?["terms"]??"",),
-                        ),
-                      ],
-                    )
-                  ),
-                ],
+          if(_loading == true){
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
               ),
+            );
+          }
+
+          return Scaffold(
+              body: Column(
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: CustomScrollView(
+                        slivers: <Widget>[
+                          SliverAppBar(
+                            pinned: true,
+                            snap: false,
+                            floating: false,
+                            expandedHeight: 200,
+                            stretch: true,
+                            forceElevated: true,
+                            flexibleSpace: LayoutBuilder(
+                                builder: (BuildContext context, BoxConstraints constraints) {
+                                  // print('constraints=' + constraints.toString());
+                                  top = constraints.biggest.height;
+                                  return FlexibleSpaceBar(
+                                      centerTitle: false,
+                                      title: AnimatedOpacity(
+                                          duration: Duration(milliseconds: 300),
+                                          opacity: top == MediaQuery.of(context).padding.top + kToolbarHeight ? 1.0 : 0.0,
+                                          //opacity: 1.0,
+                                          child: Text(
+                                            _event?["name"]??"",
+                                            style: TextStyle(overflow: TextOverflow.ellipsis),
+                                          )),
+                                      background: Image.network(CommonService.generateResourceUrl(_event?["cover_image"]??""),fit: BoxFit.cover,
+                                      )
+                                  );
+                                }
+                            ),
+                            actions: [
+                              IconButton(onPressed: (){}, icon: Icon(Icons.share))
+                            ],
+                          ),
+                          SliverToBoxAdapter(
+                              child: Column(
+                                children: [
+                                  Container(
+                                    width: double.infinity,
+                                    padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+                                    child: Text(_event?["name"], style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),),
+                                  ),
+                                  Container(
+                                      width: double.infinity,
+                                      padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+                                      child: Row(
+                                        children: [
+                                          Icon(Icons.bookmark, size: 22,color: Theme.of(context).primaryColor),
+                                          Container(width:10),
+                                          Text(_event?["event_type"]?? ""),
+                                        ],
+                                      )
+                                  ),
+
+                                  Container(
+                                      width: double.infinity,
+                                      padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+                                      child: Row(
+                                        children: [
+                                          Icon(Icons.access_time, size: 22,color: Theme.of(context).primaryColor),
+                                          Container(width:10),
+                                          Text(_event_tickets.length > 0?_event_tickets?[0]?["start_datetime"]?? "":""),
+                                        ],
+                                      )
+                                  ),
+                                  Container(
+                                      width: double.infinity,
+                                      padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+                                      child: Row(
+                                        children: [
+                                          Icon(Icons.location_pin, size: 22, color: Theme.of(context).primaryColor,),
+                                          Container(width:10),
+                                          Flexible(
+                                            flex: 1,
+                                            child:  Text(_event?["address"]?? ""),
+                                          )
+                                        ],
+                                      )
+                                  ),
+
+                                  Container(
+                                      width: double.infinity,
+                                      padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+                                      child: Row(
+                                        children: [
+                                          Icon(Icons.language, size: 22,color: Theme.of(context).primaryColor),
+                                          Container(width:10),
+                                          Text(_event?["language"]?? ""),
+                                        ],
+                                      )
+                                  ),
+
+                                  Container(
+                                      width: double.infinity,
+                                      padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+                                      child: Row(
+                                        children: [
+                                          Icon(Icons.person_2, size: 22,color: Theme.of(context).primaryColor),
+                                          Container(width:10),
+                                          Text((_event?["min_age"]?? "") + "+"),
+                                        ],
+                                      )
+                                  ),
+
+                                  Container(
+                                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                                    child: Html(data:  _event?["description"]??"",),
+                                  ),
+
+
+                                  //Artists section
+
+                                  Container(
+                                      padding: EdgeInsets.symmetric( vertical: 15),
+                                      width: double.infinity,
+                                      child: SingleChildScrollView(
+                                        scrollDirection: Axis.horizontal,
+                                        padding: EdgeInsets.symmetric(horizontal: 15,),
+                                        child: Container(
+                                          child: Row(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              children: List.generate((_artists ?? []).length, (index) =>
+                                                  Container(
+                                                    margin: EdgeInsets.symmetric(horizontal: 5),
+                                                    width: 90,
+                                                    child: Column(
+                                                      children: [
+                                                        ClipRRect(
+                                                          borderRadius: const BorderRadius.all(Radius.circular(120)),
+                                                          child: Image.network(CommonService.generateResourceUrl(_artists[index]?["image"]??""), height: 80, width: 80, fit: BoxFit.cover),
+                                                        ),
+                                                        Padding(padding: EdgeInsets.only(top: 10)),
+                                                        Text(_artists[index]?["name"]?? "", textAlign: TextAlign.center, style: TextStyle(fontSize: 12),)
+                                                      ],
+                                                    ),
+                                                  )
+                                              )
+                                          ),
+                                        ),
+                                      )
+                                  ),
+
+                                  Container(
+                                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                                    child: Html(data:  _event?["terms"]??"",),
+                                  ),
+                                ],
+                              )
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      color: Theme.of(context).primaryColor,
+                      child: InkWell(
+                          onTap: (){},
+                          child: Container(
+                            width: double.infinity,
+                            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                            child: Text("Book Now", textAlign: TextAlign.center,),
+                          )
+                      ),
+                    )
+                  ]
+              )
           );
         }
     );
